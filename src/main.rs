56e9d57a6ff128ee570cb7e5ae9ad1ea900de518
Giacomo Pantalone <giacomo.pantalone@aresys.it>
2022-11-08 10:14:26 +0100

    Running the test example of Kiss3D for wasm and binary

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,32 @@
-use rand::{thread_rng};
 extern crate kiss3d;
+//extern crate kiss3d::nalgebra as na;
 
-use kiss3d::nalgebra::{Vector3, UnitQuaternion};
-use kiss3d::window::Window;
 use kiss3d::light::Light;
+use kiss3d::scene::SceneNode;
+use kiss3d::window::{State, Window};
+use kiss3d::nalgebra::{UnitQuaternion, Vector3};
 
+struct AppState {
+    c: SceneNode,
+    rot: UnitQuaternion<f32>,
+}
 
-// fn gen_in_space() -> Vector3<f64>{
-//     let mut rng = thread_rng();
-
-//     Vector3::<f64> { x: rng.gen_range(-1.0..1.0), y: rng.gen_range(-1.0..1.0), z: rng.gen_range(-1.0..1.0)}
-// }
-
+impl State for AppState {
+    fn step(&mut self, _: &mut Window) {
+        self.c.prepend_to_local_rotation(&self.rot)
+    }
+}
 
 fn main() {
-
-    // First tests - creating a thick point cloud, relaxing it in some way and apply a delaunay mesh
-
-    // let points = vec![
-    //     gen_in_space(),
-    //     gen_in_space(),
-    //     gen_in_space(),
-    // ];
-
-    let mut window = Window::new("Kiss3d: cube");
-    let mut c      = window.add_cube(1.0, 1.0, 1.0);
+    let mut window = Window::new("Kiss3d: wasm example");
+    let mut c = window.add_cube(0.1, 0.1, 0.1);
 
     c.set_color(1.0, 0.0, 0.0);
 
     window.set_light(Light::StickToCamera);
 
     let rot = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), 0.014);
+    let state = AppState { c, rot };
 
-    while window.render() {
-        c.prepend_to_local_rotation(&rot);
-    }
-
-    println!("Hello, world!");
+    window.render_loop(state)
 }
